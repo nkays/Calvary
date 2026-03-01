e673009de58ea42bc89eaa4d4fc2fc982b472362
nkays <nelsonskays@gmail.com>
2026-03-01 07:04:44 -0500

    tests

--- src/landing_pages/tests.py
+++ src/landing_pages/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import LandingPageEntry
+from landing_pages import models
+
+class LandingPageEntryTestCase(TestCase):
+    def setUp(self):
+        LandingPageEntry.objects.create(
+            name = "Test Name",
+            slug = "test-slug",
+            email = "test@example.com",
+        )
+
+    def test_landing_page_entry_creation(self):
+        entry = LandingPageEntry.objects.get(name="Test Name")
+        self.assertEqual(entry.email, "test@example.com")
